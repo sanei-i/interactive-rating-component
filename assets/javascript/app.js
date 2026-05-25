ceb512b4fd597d6014705a1aa847bbb223bd7ed9
sanei-i <supersaneman7@gmail.com>
2026-05-25 12:43:03 +0900

    feat: get selected rating value

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,12 @@ const thankyouCard = document.querySelector(".thankyou-card");
 submitButton.addEventListener("click", (event) => {
     event.preventDefault();
 
+    // get value
+    const selectedRating = document.querySelector('input[name="rating"]:checked');
+    const selectedRatingText = document.querySelector(".valuation");
+
+    selectedRatingText.textContent = selectedRating.value;
+
     evaluationCard.classList.add("hidden")
     thankyouCard.classList.remove("hidden")
 });
